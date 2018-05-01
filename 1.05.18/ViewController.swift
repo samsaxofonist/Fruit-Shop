926fa31002a9007be7f1c4c8833307787887bbb3
Дмитрий Самусь <dima@macbook-pro-air.local>
2018-05-01 14:26:44 +0200

    Initial Commit

---
+++ 1.05.18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1.05.18
+//
+//  Created by Dima on 01.05.2018.
+//  Copyright © 2018 Samus Dimitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
